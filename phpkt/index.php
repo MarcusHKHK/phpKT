d0a67cb41c2f149a5251532dc228a58b841017b0
MarcusHKHK <marcus@hkhk.edu.ee>
2025-09-25 11:15:41 +0300

    Update index.php

--- phpkt/index.php
+++ phpkt/index.php
@@ -6,18 +6,27 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>PHP KT</title>
-        <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
-        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
         <link href="css/styles.css" rel="stylesheet" />
-        <!-- Calling jquery first -->
-        
     </head>
+    <!-- Koolituskeskus
+    Fail: 'courses.csv' (kursuse nimi; hind; kestus; kirjeldus).
+    Failid tuleb genereerida saidil https://mockaroo.com
+    Leht peab olema kujundatud Bootstrapiga ja kohanduv (responsive)
+    Navigeerimismenüü tuleb teha Bootstrap komponentidega. Menüüs vähemalt: Avaleht, Tooted/Teenused, Kalkulaator, Kontakt, Ostukorv
+    Logo (võib olla tekstina või pildina)
+    Bänner – Bootstrap carousel, mis võtab pildid automaatselt kaustast 'reklaam/'. Pildid valitakse suvaliselt, nt PHP 'glob()' abil
+    Teenused: kuvatakse failist ('.csv' või '.txt'), vähemalt 12 teenust, Bootstrap kaardina, 3 veerus ('col-md-4'). Kuvada nimi, hind, pilt, nupp "Lisa ostukorvi". Lisatud eraldi kausta 'pildid/'
+    Kalkulaator: arvutab kursuse koguhinna osalejate arvu põhjal. Lisavalik: online (–20%) või klassikoolitus (tavahind). Tulemus tuleb salvestada faili (nt 'orders.txt')
+    Kontaktileht: sisaldab Google Mapsi ja kontaktivormi
+    Leht peab olema üleval: 'liivakast.hkhk.edu.ee/~kasutaja/phpprojekt'
+    Lisaks GitHubi repositoorium
+    Esitada tuleb nii liivakasti link kui GitHubi link -->
     <body>
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container px-4 px-lg-5">
                 <a class="navbar-brand" href="#!">Kursused logo</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
@@ -37,7 +46,7 @@
                         </li>
                     </ul>
                     <form class="d-flex">
-                        <button class="btn btn-outline-dark" type="submit">
+                        <button class="btn btn-outline-light" type="submit">
                             <i class="bi-cart-fill me-1"></i>
                             Korv
                             <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
@@ -58,55 +67,57 @@
                 $filename = $files[$index];
             }
           }
-          // echo '<img class="d-block img-fluid w-100" style="height: 550px;" src="'.$dir_path."/".$filename.'" alt="'.$filename.'">';
-          echo $dir_path.'/'.$filename;
+          echo '<img src="'.$dir_path."/".$filename.'" class="d-block w-100" height="800" alt="'.$filename.'">';
         }
         ?>
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-          <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-          </div>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
           <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img src="<?php randpildid(); ?>" class="d-block w-100" alt="...">
+            <div class="carousel-item active">              
+              <?php randpildid(); ?>
             </div>
             <div class="carousel-item">
-              <img src="<?php randpildid(); ?>" class="d-block w-100" alt="...">
+              <?php randpildid(); ?>
             </div>
             <div class="carousel-item">
-              <img src="<?php randpildid(); ?>" class="d-block w-100" alt="...">
+              <?php randpildid(); ?>
             </div>
           </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon " aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
           </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
           </button>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
         <!-- Section-->
         <h1 class="text-center">Teenused</h1>
         <?php
+          // if (($open = fopen("kursused.csv", "r")) !== false) {
+          //   while (($data = fgetcsv($open, 1000, ",")) !== false) {
+          //       $array[] = $data;
+          //   }
+            
+          //   fclose($open);
+          // }
+          // echo "<pre>";
+          // echo $array[1]
+
+          // // To display array data
+          // var_dump($array);
+          // echo "</pre>";
+        ?>
+        <?php
           function kursused(){
-	          $allikas = 'kursused.csv';
-	          $minu_csv = fopen($filedir, 'r') or die ('Ei leia faili');
-            $jrk = 1;
-            while(!feof($minu_csv)){
-              $rida = fgetcsv($minu_csv, filesize($allikas),';');
-              $arv = count($rida);
-              echo $jrk.'. ';
-              $jrk++;
-              for($i = 0; $i<$arv; $i++){
-                echo $rida[$i].' ';
-              }
-              echo '<br>';
+	          $open = fopen('kursused.csv', 'r');
+            while (($open = implode($open)) !== FALSE){
+              print_r($open);
             }
-            fclose($minu_csv);
           }
+          kursused();
         ?>
         <section class="my-5 mx-5 text-center">
           <div class="row align-items-center">
@@ -156,7 +167,7 @@
         <footer class="py-5 bg-dark">
             <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2025</p></div>
         </footer>
-        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
         <script language="JavaScript" type="text/javascript" src="scripts/jquery.js"></script>
         <script language="JavaScript" type="text/javascript" src="scripts/bootstrap.min.js"></script> 
     </body>
